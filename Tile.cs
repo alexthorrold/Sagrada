cf7de2d4ff3b96e35fe50f5129b5571c2a2a4a37
alexthorrold <91945780+alexthorrold@users.noreply.github.com>
2021-10-09 15:11:13 +1300

    Test

---
+++ Tile.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Sagrada
+{
+    class Tile
+    {
+        Dice dice;
+        Color reqColor;
+        int reqNumber;
+        
+
+    }
+}
